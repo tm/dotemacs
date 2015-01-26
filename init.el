3b44b1d3f648771b6c35e00cc85ce998e5d3d3aa
Tomasz Mieloch <tomasz@mieloch.net>
2015-01-26 22:41:30 +0100

    Initial commit

---
+++ init.el
@@ -0,0 +1,326 @@
+(setq user-full-name "Tomasz Mieloch")
+(setq user-mail-address "tomasz@mieloch.net")
+
+(setenv "PATH" (concat "/usr/local/bin:/opt/local/bin:/usr/bin:/bin" (getenv "PATH")))
+(require 'cl)
+
+(add-to-list 'load-path "~/.emacs.d/elisp")
+
+                                        ; packages
+
+(load "package")
+(package-initialize)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+(setq package-archive-enable-alist '(("melpa" deft magit)))
+
+
+(defvar tm/packages '(ace-jump-mode
+                      ac-slime
+                      auctex
+                      auto-complete
+                      autopair
+                      coffee-mode
+                      color-theme-solarized
+                      emmet-mode
+                      flycheck
+                      gist
+                      git-gutter
+                      google-contacts
+                      haml-mode
+                      htmlize
+                      js2-mode
+                      magit
+                      markdown-mode
+                      neotree
+                      org
+                      paredit
+                      projectile
+                      projectile-rails
+                      reftex
+                      restclient
+                      rvm
+                      slim-mode
+                      smex
+                      yaml-mode
+                      yasnippet)
+  "Default packages")
+
+
+(defun tm/packages-installed-p ()
+  (loop for pkg in tm/packages
+        when (not (package-installed-p pkg)) do (return nil)
+        finally (return t)))
+
+(unless (tm/packages-installed-p)
+  (message "%s" "Refreshing package database...")
+  (package-refresh-contents)
+  (dolist (pkg tm/packages)
+    (when (not (package-installed-p pkg))
+      (package-install pkg))))
+
+
+
+                                        ; editor setup
+
+
+(if window-system
+    (x-focus-frame nil))
+
+(setq mac-command-modifier 'meta
+      mac-option-modifier 'none
+      default-input-method "MacOSX")
+
+(load-theme 'solarized t)
+
+(tool-bar-mode -1)
+
+(menu-bar-mode t)
+
+(scroll-bar-mode -1)
+
+(global-linum-mode -1)
+
+(column-number-mode t)
+
+(setq inhibit-splash-screen t
+      initial-scratch-message nil
+      initial-major-mode 'org-mode)
+
+(setq default-frame-alist '((left . 500) (width . 120) (height . 70)))
+
+
+(setq make-backup-files nil)
+
+(setq auto-save-default nil)
+
+(setq-default indent-tabs-mode nil)
+
+(setq tab-width 2)
+
+(set-default-font "Source Code Pro-12")
+
+(setq-default cursor-type 'hbar)
+
+
+(global-visual-line-mode 1); Proper line wrapping
+
+(setq ring-bell-function 'ignore)
+
+(delete-selection-mode t)
+(transient-mark-mode t)
+(setq x-select-enable-clipboard t)
+
+
+;; scroll one line at a time (less "jumpy" than defaults)
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
+(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
+(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
+(setq scroll-step 1) ;; keyboard scroll one line at a time
+
+
+(setq echo-keystrokes 0.1
+      use-dialog-box nil
+      visible-bell t)
+
+(show-paren-mode t); Matches parentheses and such in every mode
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(global-set-key (kbd "C-;") 'comment-or-uncomment-region)
+(global-set-key (kbd "M-/") 'hippie-expand)
+(define-key global-map (kbd "RET") 'newline-and-indent)
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+(global-set-key (kbd "M-z") 'zap-up-to-char)
+
+(global-set-key (kbd "C-s") 'isearch-forward-regexp)
+(global-set-key (kbd "C-r") 'isearch-backward-regexp)
+(global-set-key (kbd "C-M-s") 'isearch-forward)
+(global-set-key (kbd "C-M-r") 'isearch-backward)
+
+
+(setq uniquify-buffer-name-style 'forward)
+
+(setq require-final-newline t)
+
+
+                                        ; ido
+(ido-mode t)
+(ido-everywhere t)
+(setq ido-enable-flex-matching t
+      ido-use-filename-at-point nil
+      ido-auto-merge-work-directories-length 0
+      ido-use-virtual-buffers t)
+
+(setq ido-default-buffer-method 'selected-window)
+
+;; Display ido results vertically, rather than horizontally
+(setq ido-decorations (quote ("\n-> " "" "\n   " "\n   ..." "[" "]" " [No match]" " [Matched]" " [Not readable]" " [Too big]" " [Confirm]")))
+(defun ido-disable-line-truncation () (set (make-local-variable 'truncate-lines) nil))
+(add-hook 'ido-minibuffer-setup-hook 'ido-disable-line-truncation)
+(defun ido-define-keys () ;; C-n/p is more intuitive in vertical layout
+  (define-key ido-completion-map (kbd "C-n") 'ido-next-match)
+  (define-key ido-completion-map (kbd "C-p") 'ido-prev-match))
+(add-hook 'ido-setup-hook 'ido-define-keys)
+
+
+                                        ; tildify
+(setq tildify-pattern-alist
+      '((t "\\([,:;(][ \t]*[a]\\|\\<[AOIUWZaoiuwz]\\)\\([ \t]+\\|[ \t]*\n[ \t]*\\)\\(\\w\\|[([{\\]\\|<[a-zA-Z]\\)" 2)))
+
+                                        ; css
+(setq css-indent-offset 2)
+(add-hook 'css-mode-hook '(lambda ()
+                            (local-set-key (kbd "RET") 'newline-and-indent)))
+
+                                        ; calendar setup
+(setq calendar-week-start-day 0
+      calendar-day-name-array ["niedziela" "poniedziałek" "wtorek" "środa"
+                               "czwartek" "piątek" "sobota"]
+      calendar-month-name-array ["styczeń" "luty" "marzec" "kwiecień" "maj"
+                                 "czerwiec" "lipiec" "sierpień" "wrzesień"
+                                 "październik" "listopad" "grudzień"])
+
+                                        ; ruby-mode
+(add-hook 'ruby-mode-hook
+          '(lambda ()
+             (define-key ruby-mode-map (kbd "RET") 'reindent-then-newline-and-indent)
+             (electric-indent-mode)))
+
+;; Rake files are ruby, too, as are gemspecs, rackup files, etc.
+(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
+
+                                        ;Textmate like Command-RET
+(defun tm-mate-command-return ()
+  "TextMate like Command-RET"
+  (interactive)
+  (move-end-of-line nil)
+  (newline-and-indent))
+
+(global-set-key (kbd "M-RET") 'tm-mate-command-return)
+
+
+                                        ; aspell
+(setq ispell-program-name "aspell")
+
+                                        ; magit
+(global-set-key (kbd "C-x g") 'magit-status)
+
+
+                                        ; org
+(setq org-log-done t
+      org-todo-keywords '((sequence "TODO" "INPROGRESS" "DONE"))
+      org-todo-keyword-faces '(("INPROGRESS" . (:foreground "blue" :weight bold))))
+(add-hook 'org-mode-hook
+          (lambda ()
+            (flyspell-mode)))
+
+(global-set-key (kbd "C-c a") 'org-agenda)
+(setq org-agenda-show-log t
+      org-agenda-todo-ignore-scheduled t
+      org-agenda-todo-ignore-deadlines t)
+(setq org-agenda-files (list "~/Dropbox/org/personal.org"
+                             "~/Dropbox/org/groupon.org"))
+
+
+                                        ; smex
+(setq smex-save-file (expand-file-name ".smex-items" user-emacs-directory))
+(smex-initialize)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+
+                                        ; auto-complete
+(ac-config-default)
+
+
+                                        ; indentation and buffer clenaup
+
+(defun untabify-buffer ()
+  (interactive)
+  (untabify (point-min) (point-max)))
+
+(defun indent-buffer ()
+  (interactive)
+  (indent-region (point-min) (point-max)))
+
+(defun cleanup-buffer ()
+  "Perform a bunch of operations on the whitespace content of a buffer."
+  (interactive)
+  (indent-buffer)
+  (untabify-buffer)
+  (delete-trailing-whitespace))
+
+(defun cleanup-region (beg end)
+  "Remove tmux artifacts from region."
+  (interactive "r")
+  (dolist (re '("\\\\│\·*\n" "\W*│\·*"))
+    (replace-regexp re "" nil beg end)))
+
+(global-set-key (kbd "C-x M-t") 'cleanup-region)
+(global-set-key (kbd "C-c n") 'cleanup-buffer)
+
+
+
+                                        ; rvm
+(rvm-use-default)
+
+                                        ; yaml
+(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
+
+
+                                        ; js2-mode
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+
+(setq js2-mode-hook
+      '(lambda () (progn
+                    (set-variable 'js2-basic-offset 2))))
+
+
+                                        ; ace-jump-mode
+(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+
+
+                                        ; neotree
+(global-set-key [f8] 'neotree-toggle)
+
+                                        ; projectile
+(projectile-global-mode)
+
+                                        ; projectile-rails
+(add-hook 'projectile-mode-hook 'projectile-rails-on)
+
+                                        ; org-drill
+(require 'org-drill)
+
+                                        ; markdown
+
+(defun markdown-preview-file ()
+  "use Marked 2 to preview the current file"
+  (interactive)
+  (shell-command
+   (format "open -a 'Marked 2.app' %s"
+           (shell-quote-argument (buffer-file-name))))
+  )
+(global-set-key "\C-cm" 'markdown-preview-file)
+
+                                        ; macro
+;; macros
+(global-set-key [f10]  'start-kbd-macro)
+(global-set-key [f11]  'end-kbd-macro)
+(global-set-key [f12]  'call-last-kbd-macro)
+
+                                        ; git-gutter
+(global-git-gutter-mode +1)
+
+                                        ; custom set variables
+(custom-set-variables '(coffee-tab-width 2))
